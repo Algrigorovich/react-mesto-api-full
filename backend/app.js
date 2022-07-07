e930cce31868c811bd82b0418406455067fef37c
Alexander Grigorovich <crit1k@yandex.ru>
2022-07-07 14:54:41 +0000

    fix

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-mongoose.connect(DB_CONN);
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const options = {
   origin: [
