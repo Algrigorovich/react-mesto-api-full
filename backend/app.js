c488fa3eb4cd47130038a8227378b9389e1a3344
Alexander Grigorovich <crit1k8912@gmail.com>
2022-07-07 16:34:10 +0300

    add rate-limiter, helmet

--- backend/app.js
+++ backend/app.js
@@ -2,38 +2,46 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
-const { celebrate, Joi } = require('celebrate');
+const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const handleError = require('./middlewares/handle-error');
-const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-err');
-const urlRegexp = require('./constants/regexp-url');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, DB_LINK } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DB_LINK);
 
 const options = {
   origin: [
     'http://localhost:3000',
     'http://algrigorovich.student.nomoredomains.sbs',
-    'https://algrigorovich.student.nomoredomains.sbs'
+    'https://algrigorovich.student.nomoredomains.sbs',
   ],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
 app.use(cors(options));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+app.use(helmet());
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
@@ -42,22 +50,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlRegexp),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
+app.use('/', authRouter);
 
 app.use(auth);
 
---
+++ backend/routes/auth.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const urlRegexp = require('../constants/regexp-url');
+
+const {
+  login,
+  createUser,
+} = require('../controllers/users');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlRegexp),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+module.exports = router;
