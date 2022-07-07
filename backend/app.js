9ed512c5f7c7d2005e8569526a8b4c6cd9df624f
Alexander Grigorovich <crit1k8912@gmail.com>
2022-07-07 18:32:12 +0300

    fix routs

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,16 @@ const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const cors = require('cors');
+const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
-const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const handleError = require('./middlewares/handle-error');
 const NotFoundError = require('./errors/not-found-err');
+const { login, createUser } = require('./controllers/users');
+const urlRegexp = require('./constants/regexp-url');
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -48,7 +50,23 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.use('/', authRouter);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlRegexp),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
 app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
